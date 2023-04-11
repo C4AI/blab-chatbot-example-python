e1fd0a0ba368e850447c7f7d7e62b469866bf592
Vinícius <matosvb@gmail.com>
2023-04-11 19:27:50 -0300

    Fix documentation formatting

--- settings_example_TEMPLATE.py
+++ settings_example_TEMPLATE.py
@@ -7,20 +7,20 @@ from typing import Any
 
 BLAB_CONNECTION_SETTINGS: dict[str, str | int] = {
 
-    # address of the local HTTP server that the controller will connect to
+    # Address of the local HTTP server that the controller will connect to:
     "BOT_HTTP_SERVER_HOSTNAME": "127.0.0.1",
 
-    # port of the aforementioned server
+    # Port of the aforementioned server:
     "BOT_HTTP_SERVER_PORT": 25229,
 
-    # BLAB Controller address for WebSocket connections
+    # BLAB Controller address for WebSocket connections:
     "BLAB_CONTROLLER_WS_URL": "ws://localhost:8000",
 
 }
 
 EXAMPLE_SETTINGS: dict[str, Any] = {
 
-    # text sent to the user when a conversation starts
+    # Text sent to the user when a conversation starts:
     'GREETING_TEXT': "Hey!",
 
 }
